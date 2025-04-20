b44aac513e2e0bfe949964c207f8de609bdb5051
mmandelyn <mmcginnis1@una.edu>
2025-04-19 21:49:37 -0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Mandy McGinnis in CIS 476'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
